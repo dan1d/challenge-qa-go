d815a6194ccf5302976246c124bafbe4670be016
Daniel Dominguez <danielfromarg@gmail.com>
2024-09-09 18:59:10 -0300

    traefik...

--- aqa/cypress/e2e/message.cy.js
+++ aqa/cypress/e2e/message.cy.js
@@ -1,7 +1,7 @@
 describe('New Message Test', () => {
   it('should send a message successfully', () => {
     cy.login();
-    cy.get('textarea').should('be.visible').type('Hello, this is a test message!');
+    cy.get('textarea').should('be.visible').type('Hello, this is a test message! from e2e.');
     cy.get('button[name=send]').click();
     cy.contains('Hello, this is a test message!');
   });
