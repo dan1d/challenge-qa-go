01696467b259be3daa4b893c2a4865d8105d7b85
Daniel Dominguez <danielfromarg@gmail.com>
2024-09-09 17:50:21 -0300

    traefik...

--- aqa/server.js
+++ aqa/server.js
@@ -8,6 +8,11 @@ const app = express();
 // const port = process.env.PORT || 3002;
 const port = 3005;
 
+app.use((req, res, next) => {
+  console.log(`Received ${req.method} request for ${req.url}`);
+  next();
+});
+
 const allowedOrigins = ['https://app.qa-challenge.dan1d.dev', 'https://aqa.qa-challenge.dan1d.dev'];
 app.use(cors({
   origin: allowedOrigins,
